d770caff6e6992f4fc344f1383a0ca6e9ff4c4e8
Zane_Goodman <zgoodman1776@gmail.com>
2022-12-17 13:17:13 -0500

    completed

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const double = (arr) => arr.map((val) => val * 2 )
+
+
+
+
+// const squareAndFindEvens = ((numbers)=> {
+//     const squares = numbers.map((num) => num ** 2 );
+//     const evens = squares.filter((square) => square % 2 === 0);
+//     return evens;
+// })
+
+const squareAndFindEvens = (numbers) => numbers.map((val) => val ** 2).filter((square)=> square % 2 === 0)
